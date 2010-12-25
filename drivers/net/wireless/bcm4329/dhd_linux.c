8c3677a61bdf16724862cf6976b9aab7747bbedb
cvpcs <root@cvpcs.org>
2010-12-25 15:29:13 -0600

    Revert "net: wireless: bcm4329: Fix unregister_early_suspend() in dhd_detach()"
    This reverts commit f72bac5e5ff68b19c3d0b922c2f5b7b3fa12ad0a.

--- drivers/net/wireless/bcm4329/dhd_linux.c
+++ drivers/net/wireless/bcm4329/dhd_linux.c
@@ -2173,8 +2173,7 @@ dhd_detach(dhd_pub_t *dhdp)
 			int i;
 
 #if defined(CONFIG_HAS_EARLYSUSPEND)
-			if (dhd->early_suspend.suspend)
-				unregister_early_suspend(&dhd->early_suspend);
+			unregister_early_suspend(&dhd->early_suspend);
 #endif	/* defined(CONFIG_HAS_EARLYSUSPEND) */
 #if defined(CONFIG_WIRELESS_EXT)
 			/* Attach and link in the iw */
